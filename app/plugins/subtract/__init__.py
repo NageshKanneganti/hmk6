cb8e71859ffdb4acaf5524eb9f6488b824caca8e
NageshKanneganti <nk726@njit.edu>
2024-03-06 10:09:07 -0600

    Refactored operations plugin commands & added logging

--- app/plugins/subtract/__init__.py
+++ app/plugins/subtract/__init__.py
@@ -2,6 +2,7 @@
 from app.commands import Command
 from app.calculator import Calculator
 from app.utils.validation import validate_decimal_input
+import logging
 
 class SubtractCommand(Command):
     '''A command class to perform subtraction.'''
@@ -11,8 +12,11 @@ class SubtractCommand(Command):
 
         This method prompts the user to enter two numbers and performs subtraction.
         '''
+        logging.info("Command 'subtract' from plugin 'menu' selected.")
         num1 = validate_decimal_input("Enter the first number: ")
         num2 = validate_decimal_input("Enter the second number: ")
+
+        logging.info("Performing subtraction...")
         result = Calculator.subtract(num1, num2)
         print(f"The result of {num1} - {num2} is: {result}")
         return result
